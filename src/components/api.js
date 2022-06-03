05d91bb705df57bf79a14804c1bec15f925ea1f4
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-06-02 21:57:02 -0600

    Change returns to return parsed data

--- src/components/api.js
+++ src/components/api.js
@@ -1,3 +1,5 @@
+import APIParser from './apiParser';
+
 const API = (() => {
   const getCityOptions = async (city) => {
     const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=6f84b948b64a49ec4618c61cb10bc7dc`, { mode: 'cors' });
@@ -6,7 +8,7 @@ const API = (() => {
     if (data.length < 1) {
       throw new Error(400);
     }
-    return data;
+    return APIParser.parseCities(data);
   };
 
   const getCityCountryOptions = async (city, code) => {
@@ -16,14 +18,14 @@ const API = (() => {
     if (data.length < 1) {
       throw new Error(400);
     }
-    return data;
+    return APIParser.parseCities(data);
   };
 
   const getWeatherData = async (city) => {
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=6f84b948b64a49ec4618c61cb10bc7dc`, { mode: 'cors' });
       const data = response.json();
-      return data;
+      return APIParser.parseWeather(data);
     } catch (e) {
       return e;
     }
