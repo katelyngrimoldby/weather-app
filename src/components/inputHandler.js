cd00dce7eb3c673b5e90afed8ba4c6d8f5b24d4a
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-06-03 13:40:50 -0600

    Add try/catch and ui methods

--- src/components/inputHandler.js
+++ src/components/inputHandler.js
@@ -1,4 +1,5 @@
 import API from './api';
+import UI from './ui';
 
 const InputHandler = (() => {
   const input = document.getElementById('city');
@@ -7,18 +8,22 @@ const InputHandler = (() => {
   submit.addEventListener('click', async () => {
     const inputValue = input.value;
     let data;
-
-    if (inputValue.includes(',')) {
-      const arr = inputValue.split(',', 2);
-      const city = arr[0].trim();
-      const country = arr[1].trim();
-      data = await API.getCityCountryOptions(city, country);
-    } else {
-      const city = inputValue.trim();
-      data = await API.getCityOptions(city);
+    try {
+      if (inputValue.includes(',')) {
+        const arr = inputValue.split(',', 2);
+        const city = arr[0].trim();
+        const country = arr[1].trim();
+        data = await API.getCityCountryOptions(city, country);
+      } else {
+        const city = inputValue.trim();
+        data = await API.getCityOptions(city);
+      }
+      UI.clearContent();
+      UI.createCityCards(data);
+    } catch (e) {
+      UI.displayError();
     }
     input.value = '';
-    console.log(data);
   });
 })();
 
