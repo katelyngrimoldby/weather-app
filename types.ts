b5ea5cafc39a61dc4ad7cc7216c8145de33deb31
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-03 15:38:42 -0600

    Add types for forecast

--- types.ts
+++ types.ts
@@ -69,4 +69,64 @@ interface locationData {
   lon: number,
 }
 
-export type {weatherData, geoData, locationData};
+interface forecastData {
+  cod: string,
+  message: number,
+  cnt: number;
+  list: forecastEntry[],
+  city: {
+    id: number,
+    name: string,
+    coord: {
+      lat: number,
+      lon: number,
+    }
+    country: string,
+    timezone: number,
+    sunrise: number,
+    sunset: number,
+  }
+}
+
+interface forecastEntry {
+  dt: number,
+  main: {
+    temp: number,
+    feels_like: number,
+    temp_min: number,
+    temp_max: number,
+    pressure: number,
+    sea_level: number,
+    grnd_level: number,
+    humidity: number,
+    temp_kf: number,
+  },
+  weather: {
+    id: number,
+    main: string,
+    description: string,
+    icon: string,
+  }[],
+  clouds: {
+    all: number,
+  },
+  wind: {
+    speed: number,
+    deg: number,
+    gust: number,
+  },
+  visibility: number,
+  pop: number,
+  rain: {
+    '3h': number,
+  },
+  snow: {
+    '3h': number,
+  },
+  sys: {
+    pod: string,
+  },
+  dt_txt: string,
+}
+
+export type {weatherData, geoData, locationData, forecastData, forecastEntry};
